580d2960e3bdd468d6c27652323dca568a5f5004
Olle Haerstedt <olle.haerstedt@limesurvey.org>
2022-04-07 22:28:28 +0200

    Move code to plugin

--- application/controllers/survey/index.php
+++ application/controllers/survey/index.php
@@ -607,17 +607,6 @@ class Index extends CAction
 
         sendCacheHeaders();
 
-        if (!$isSurveyActive) {
-            // Register React dev environment for edit-in-place in preview
-            // @see https://reactjs.org/docs/add-react-to-a-website.html#quickly-try-jsx
-            // @see https://raw.githubusercontent.com/reactjs/reactjs.org/main/static/html/single-file-example.html
-            // @todo Not recommended for production use (but kind of OK since traffic will be low)
-            App()->getClientScript()->registerScriptFile('https://unpkg.com/react@18/umd/react.development.js');
-            App()->getClientScript()->registerScriptFile('https://unpkg.com/react-dom@18/umd/react-dom.development.js');
-            App()->getClientScript()->registerScriptFile('https://unpkg.com/@babel/standalone/babel.min.js');
-            App()->getClientScript()->registerScriptFile(App()->getConfig('generalscripts') . 'edit_in_place.js', null, ['type' => 'text/babel']);
-        }
-
         //Send local variables to the appropriate survey type
         unset($redata);
         $redata = compact(array_keys(get_defined_vars()));
--- application/core/plugins/EditInPlace/EditInPlace.php
+++ application/core/plugins/EditInPlace/EditInPlace.php
@@ -18,6 +18,16 @@ class EditInPlace extends PluginBase
     public function beforeSurveyPage()
     {
         $event = $this->getEvent();
-        $event->set('template', __DIR__ . '/moo.html');
+        $survey = Survey::model()->findByPk($event->get('surveyId'));
+        if (!empty($survey) && $survey->active === 'N') {
+            // Register React dev environment for edit-in-place in preview
+            // @see https://reactjs.org/docs/add-react-to-a-website.html#quickly-try-jsx
+            // @see https://raw.githubusercontent.com/reactjs/reactjs.org/main/static/html/single-file-example.html
+            // @todo Not recommended for production use (but kind of OK since traffic will be low)
+            App()->getClientScript()->registerScriptFile('https://unpkg.com/react@18/umd/react.development.js');
+            App()->getClientScript()->registerScriptFile('https://unpkg.com/react-dom@18/umd/react-dom.development.js');
+            App()->getClientScript()->registerScriptFile('https://unpkg.com/@babel/standalone/babel.min.js');
+            App()->getClientScript()->registerScriptFile(App()->getConfig('generalscripts') . 'edit_in_place.js', null, ['type' => 'text/babel']);
+        }
     }
 }
